48b2fb7ba25b47f8941921b823ca30c2e5c1a94f
Salvador Moret <a@salvadormoret.com>
2014-05-15 15:28:25 +0200

    Comenzando los test del capítulo 33.

--- app/src/GSD/Providers/TodoServiceProvider.php
+++ app/src/GSD/Providers/TodoServiceProvider.php
@@ -11,14 +11,16 @@ class TodoServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app['todo'] = $this->app->share(function()
+        $this->app['todo'] = $this->app->share( function()
         {
             return new TodoManager;
-        });
+        } );
 
-        $this->app->bind('GSD\Entities\ListInterface', 'GSD\Entities\TodoList');
-        $this->app->bind('GSD\Entities\TaskInterface', 'GSD\Entities\Task');
-        $this->app->bind('GSD\Entities\TaskCollectionInterface',
-            'GSD\Entities\TaskCollection');
+        $this->app->bind( 'GSD\Entities\ListInterface', 'GSD\Entities\TodoList' );
+        $this->app->bind( 'GSD\Entities\TaskInterface', 'GSD\Entities\Task' );
+        $this->app->bind( 'GSD\Entities\TaskCollectionInterface',
+            'GSD\Entities\TaskCollection' );
+        $this->app->bind( 'GSD\Repositories\RepositoryInterface',
+            'GSD\Repositories\Repository' );
     }
 }
--- app/src/GSD/Repositories/Repository.php
+++ app/src/GSD/Repositories/Repository.php
@@ -49,4 +49,149 @@ class Repository implements RepositoryInterface
         }
         return false;
     }
+
+    /**
+     * Does the todo list exist?
+     *
+     * @param  string  $id       ID of the list
+     * @param  boolean $archived Check for archived lists only?
+     *
+     * @return boolean
+     */
+    public function exists( $id, $archived = false )
+    {
+        $file = $this->fullpath( $id, $archived );
+        return file_exists( $file );
+    }
+
+    /**
+     * Return the ids of all the lists
+     * @param boolean $archived Return archived ids or unarchived?
+     * @return array of list ids
+     */
+    public function getAll( $archived = false )
+    {
+        $match = $this->path;
+        if ( $archived )
+        {
+            $match .= 'archived/';
+        }
+        $match .= '*' . $this->extension;
+        $files = glob( $match );
+        $ids = array();
+        foreach ($files as $file)
+        {
+            $ids[] = basename( $file, $this->extension );
+        }
+        return $ids;
+    }
+
+    /**
+     * Load a Todolist from it's id
+     *
+     * @param  string  $id       ID of the list
+     * @param  boolean $archived Load an archived list?
+     *
+     * @return ListInterface            The list
+     * @throws InvalidArgumentException If $id not found
+     */
+    public function load( $id, $archived = false )
+    {
+        if ( ! $this->exists( $id, $archived ) )
+        {
+            throw new \InvalidArgumentException( "List with id = $id, archived = $archived not found" );
+        }
+        $lines = file( $this->fullpath( $id, $archived ) );
+
+        // Pull title
+        $title = array_shift( $lines );
+        $title = trim( substr( $title, 1 ) );
+
+        // Pull subtitle
+        if ( count( $lines ) && $lines[0][0] == '(' )
+        {
+            $subtitle = trim( array_shift( $lines ) );
+            $subtitle = ltrim( $subtitle, '(' );
+            $subtitle = rtrim( $subtitle, ')' );
+        }
+
+        // Setup the list
+        $list = \App::make( 'GSD\Entities\ListInterface' );
+        $list->set( 'id', $id );
+        $list->set( 'title', $title );
+        if ( ! empty( $subtitle ) )
+        {
+            $list->set( 'subtitle', $subtitle );
+        }
+        $list->set( 'archived', $archived );
+
+        // And add the tasks
+        foreach ( $lines as $line )
+        {
+            $line = trim( $line );
+            if ( $line )
+            {
+                $list->taskAdd( $line );
+            }
+        }
+
+        return $list;
+    }
+
+    /**
+     * Save a Todolist
+     *
+     * @param  ListInterface $list The TODO list
+     *
+     * @return boolean              True if successful
+     */
+    public function save( ListInterface $list )
+    {
+        $id = $list->get( 'id' );
+        $archived = $list->get( 'archived' );
+        $build = array();
+        $build[] = '# ' . $list->get( 'title' );
+        $subtitle = $list->get( 'subtitle' );
+        if ( $subtitle )
+        {
+            $build[] = "($subtitle)";
+        }
+        $lastType = '';
+        $tasks = $list->tasks();
+        foreach ( $tasks as $task )
+        {
+            $task = ( string )$task;
+            $type = $task[0];
+            if ( $type != $lastType )
+            {
+                $build[] = '';   // Blank line between types of tasks
+                $lastType = $type;
+            }
+            $build[] = $task;
+        }
+        $content = join( "\n", $build );
+        $filename = $this->fullpath( $id, $archived );
+        $result = file_put_contents( $filename, $content );
+
+        return $result !== false;
+    }
+
+    /**
+     * Return the path to the list file
+     *
+     * @param  string $id       ID of the list
+     * @param  boolean $archived True if archived
+     *
+     * @return string           The path to the list file
+     */
+    protected function fullpath( $id, $archived )
+    {
+        $path = $this->path;
+        if ( $archived )
+        {
+            $path .= 'archived/';
+        }
+        $path .= $id . $this->extension;
+        $return $path;
+    }
 }
